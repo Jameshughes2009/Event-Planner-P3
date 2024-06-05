7730ad9b7e9a7c193d2bc68dd468e222b381c02d
James Hughes <jameshughes2009@hotmail.com>
2024-06-05 19:15:50 -0400

    building final repo and server functions

---
+++ server/seeds/seed.js
@@ -0,0 +1,21 @@
+const db = require('../config/connection');
+const { User, Event, Comment} = require("../models");
+
+
+const commentData = require("./commentData.json")
+const userInfo = require("./userInfo.json")
+const eventInfo = require('./eventInfo.json')
+
+
+db.once("open", async () => {
+    await User.deleteMany({});
+    // await Event.deleteMany({});
+
+    const users = await User.insertMany(userInfo);
+    const events = await Event.insertMany(eventInfo)
+    // const comments = await Comment.insertMany(commentData)
+
+    console.log("All Data Seeded");
+    process.exit(0);
+})
+
