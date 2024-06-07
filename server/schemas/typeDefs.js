642d74c24ad546bed12bceaf780823de25a04957
James Hughes <jameshughes2009@hotmail.com>
2024-06-07 16:56:35 -0400

    adding all backend functionailty currenlty developed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,5 +23,27 @@ const typeDefs = gql`
         user: User
         date: String!
     }
+
+    type Query {
+        users: [User]!
+        user(id: ID!): User
+        events: [Event]!
+        event(id: ID!): Event
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        createEvent(title: String!, description: String!, cost: Float!, location: String!, user: ID!, date: String!): Event
+        login(email: String!, password: String!): Auth
+        updateEvent(id: ID!, title: String, description: String, cost: Float, location: String, date: String): Event
+        deleteEvent(id: ID!): Boolean
+    }
+
+
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+
 `;
 module.exports = typeDefs
