5969aefef142248f208e76ad0ea749033deb317d
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-06-08 14:03:14 -0400

    Adding Contacts.js code

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS for styling
+import './style.css'; // Importing custom CSS for additional styling
+
