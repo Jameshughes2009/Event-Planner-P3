bb94a5d91d05fc53bb9c87fe681682632ba6196f
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-06-08 14:31:18 -0400

    Styling added also Code for UpdateEvt.js

---
+++ client/src/components/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // Specify the paths to all of the template files in your project
+    content: [],
+    
+    theme: {
+      // Extend the default theme to add custom styles
+      extend: {},
+    },
+    
+    // Add any plugins that you're using here
+    plugins: [],
+  }
+  
