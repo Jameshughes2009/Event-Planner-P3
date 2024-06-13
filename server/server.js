1f15916d18c0219bca9d49ffdf7ffedb5c2e7b18
James Hughes <jameshughes2009@hotmail.com>
2024-06-12 23:26:00 -0400

    deployment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express")
-const path = require("path")
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const { typeDefs, resolvers } = require('./schemas')
-const db = require('./config/connection')
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,22 +15,22 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
-  }
+}
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app });
+
     db.once("open", () => {
         app.listen(PORT, () => {
-            console.log(`We are Succesfully Running on port ${PORT}!`);
+            console.log(`We are successfully running on port ${PORT}!`);
             console.log(
-                `Connect to Graph testing http://localhost:${PORT}${server.graphqlPath}`
+                `Connect to GraphQL at http://localhost:${PORT}${server.graphqlPath}`
             );
-        })
-    })
+        });
+    });
 };
 
 startApolloServer(typeDefs, resolvers);
