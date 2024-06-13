63adf33993d5cd30626d1f0dce3f5ef5bd4a0ac9
James Hughes <jameshughes2009@hotmail.com>
2024-06-13 09:58:32 -0400

    more testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/EventPlannerP3")
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/EventPlannerP3", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // Other options if needed
-});
+module.exports = mongoose.connection;
 
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-    console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-module.exports = db;
